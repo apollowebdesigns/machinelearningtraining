22d3da9bee89218d8ba31a462128bdebb5ec4259
Andy Evans <andrewevans0@outlook.com>
2018-04-30 19:17:09 +0100

    adding a third video

--- video_one.py
+++ video_one.py
@@ -1,5 +1,14 @@
 from sklearn import tree
-features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+import redis
+import pickle
+
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+p_features = pickle.dumps([[140, 1], [130, 1], [150, 0], [170, 0]])
+
+r.set('data', p_features)
+
+# using redis to store data and then recall it
+features = pickle.loads(r.get('data'))
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)

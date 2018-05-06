a7c47a0b2d0039e31c088b2ae344d6e03d4b0e0d
Andy Evans <andrewevans0@outlook.com>
2018-05-06 17:04:20 +0100

    more

--- video_one.py
+++ video_one.py
@@ -2,8 +2,14 @@ from sklearn import tree
 import redis
 import pickle
 
+smooth = 1
+bumpy = 0
+
+apple = 0
+orange = 1
+
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-p_features = pickle.dumps([[140, 1], [130, 1], [150, 0], [170, 0]])
+p_features = pickle.dumps([[140, smooth], [130, smooth], [150, bumpy], [170, bumpy]])
 
 r.set('data', p_features)
 
@@ -12,5 +18,14 @@ features = pickle.loads(r.get('data'))
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print(clf.predict([[160, 0]]))
-print(clf.predict([[140, 0]]))
+
+def predicter(inputData):
+    if clf.predict([inputData]) == 0:
+        return 'apple'
+    else:
+        return 'orange'
+
+# print(clf.predict([[160, 0]]))
+# print(clf.predict([[170, 0]]))
+
+print(predicter([100, 0]))
